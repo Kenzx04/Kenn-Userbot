ebaa7c7c0c3817c67f0f0e67ea487f9fee89a4ad
Kenzx04 <85724834+Kenzx04@users.noreply.github.com>
2021-06-12 01:36:03 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -10,25 +10,37 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^.P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Assalamu'alaikum")
+    await typew.edit("Assalamu'alaikum Kak hehe...")
 
 
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Assalamu'alaikum")
+    await typew.edit("Assalamu'alaikum ngentot...")
 
 
 @register(outgoing=True, pattern='^.L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Wa'alaikumussalam")
+    await typew.edit("Wa'alaikumussalam Kak hehe...")
 
 
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Wa'alaikumussalam")
+    await typew.edit("Wa'alaikumussalam kontol...")
+
+
+@register(outgoing=True, pattern='^M(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Bhahahahaks Lucu Anjeng!!!!")
+
+
+@register(outgoing=True, pattern='^K(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("LU KONTOOOOLLLLLLL!!!!")
 
 
 CMD_HELP.update({
@@ -37,4 +49,7 @@ CMD_HELP.update({
 \nUsage: Untuk Memberi salam.\
 \n\n`.L`\
 \nUsage: Untuk Menjawab Salam."
-})
+\n\n`M`\
+\nUsage: .\
+\n\n`K`\
+\nUsage: ."
